42ea8bd69d877e5d413a5c94c364470577c17ed9
xdrie <xdrie@alt.icu>
2020-06-07 02:32:09 -0700

    run sync in loop

--- MatrixSyncThread.cs
+++ MatrixSyncThread.cs
@@ -3,52 +3,46 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace libMatrix
-{
-    public partial class MatrixAPI
-    {
+namespace libMatrix {
+    public partial class MatrixAPI {
         private bool isRunningSync = false;
 
         public bool shouldFullSync = false;
-        
+
         public TimeSpan period = TimeSpan.FromMilliseconds(250);
 
         public CancellationTokenSource syncThreadCts;
 
         public Task StartSyncThreads() {
             syncThreadCts = new CancellationTokenSource();
-            
+
             return Task.Run(async () => {
-                if (!isRunningSync)
-                {
-                    isRunningSync = true;
-                    try
-                    {
-                        if (shouldFullSync)
-                        {
-                            await ClientSync(true, true);
-                            shouldFullSync = false;
+                while (!syncThreadCts.IsCancellationRequested) {
+                    if (!isRunningSync) {
+                        isRunningSync = true;
+                        try {
+                            if (shouldFullSync) {
+                                await ClientSync(true, true);
+                                shouldFullSync = false;
+                            }
+                            else
+                                await ClientSync(true);
                         }
-                        else
-                            await ClientSync(true);
+                        catch (Exception e) {
+                            Debug.WriteLine("Sync Exception: " + e.Message);
+                        }
+
+                        FlushMessageQueue();
+
+                        isRunningSync = false;
                     }
-                    catch (Exception e)
-                    {
 
-                        Debug.WriteLine("Sync Exception: " + e.Message);
-                    }
-
-                    FlushMessageQueue();
-
-                    isRunningSync = false;
+                    await Task.Delay(period, syncThreadCts.Token);
                 }
-
-                await Task.Delay(period, syncThreadCts.Token);
             }, syncThreadCts.Token);
         }
 
-        public void StopSyncThreads()
-        {
+        public void StopSyncThreads() {
             syncThreadCts.Cancel();
             FlushMessageQueue();
         }
